a419dcb1e66db117db0b746f6f00c7e5217aa152
Metilka <136602389+Metilka@users.noreply.github.com>
2025-09-29 11:54:21 +0400

    5 task with MySecondClass moved to myfirstpackage

---
+++ Task5/myfirstpackage/MySecondClass.java
@@ -0,0 +1,29 @@
+package myfirstpackage;
+
+public class MySecondClass {
+  private int a;
+  private int b;
+
+  public MySecondClass(int a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public int getA() {
+    return a;
+  }
+  public int getB() {
+    return b;
+  }
+
+  public void setA(int a) {
+    this.a = a;
+  }
+  public void setB(int b) {
+    this.b = b;
+  }
+
+  public int subtraction() {
+    return a - b;
+  }
+}
